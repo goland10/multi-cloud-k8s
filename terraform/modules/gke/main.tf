f97640337bb3f9d312c82a39ab8970a441c86bc2
Golan <goland1@gmail.com>
2026-01-08 07:59:06 +0200

    impersonate_gke_nodes

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -7,11 +7,11 @@ resource "google_service_account" "gke_nodes" {
   display_name = "GKE Nodes"
 }
 
-#resource "google_service_account_iam_member" "impersonate_gke_nodes" {
-#  service_account_id = google_service_account.gke_nodes.name
-#  role   = "roles/iam.serviceAccountAdmin"
-#  member = "serviceAccount:${data.google_service_account.github_terraform.email}"
-#}
+resource "google_service_account_iam_member" "impersonate_gke_nodes" {
+  service_account_id = google_service_account.gke_nodes.name
+  role   = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:${data.google_service_account.github_terraform.email}"
+}
 
 resource "google_container_cluster" "this" {
   name     = var.cluster_name
