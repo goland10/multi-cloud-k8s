ac1b64975e03944868061b70d89290920b31df6b
Golan <goland1@gmail.com>
2026-01-09 01:02:14 +0200

    Hope auth fixed

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -8,18 +8,18 @@ module "network" {
   region = var.region
 }
 
-#module "gke" {
-#  source = "../../modules/gke"
-#
-#  project_id = var.project_id
-#  region     = var.region
-#
-#  cluster_name = local.cluster_name
-#  environment  = local.environment
-#
-#  network    = module.network.vpc_id
-#  subnetwork = module.network.subnet_id
-#
-#  pods_range_name     = "pods"
-#  services_range_name = "services"
-#}
+module "gke" {
+  source = "../../modules/gke"
+
+  project_id = var.project_id
+  region     = var.region
+
+  cluster_name = local.cluster_name
+  environment  = local.environment
+
+  network    = module.network.vpc_id
+  subnetwork = module.network.subnet_id
+
+  pods_range_name     = "pods"
+  services_range_name = "services"
+}
