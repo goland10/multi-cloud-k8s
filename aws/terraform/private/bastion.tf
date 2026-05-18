29b5f5af1698dd017053de1ccefb7772b7a5db0c
Golan <goland1@gmail.com>
2026-05-18 23:24:57 +0300

    add inbound rule to cluster_primary_security_group

--- aws/terraform/private/bastion.tf
+++ aws/terraform/private/bastion.tf
@@ -34,15 +34,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_eice" {
 ##########################################
 ## Cluster security group
 ##########################################
-#resource "aws_vpc_security_group_ingress_rule" "allow_bastion_to_eks" {
-#  security_group_id = module.eks.cluster_primary_security_group_id
-#
-#  referenced_security_group_id = aws_security_group.bastion_sg.id
-#  from_port                    = 443
-#  to_port                      = 443
-#  ip_protocol                  = "tcp"
-#  description                  = "Allow Bastion host to communicate with EKS API server"
-#}
+resource "aws_vpc_security_group_ingress_rule" "allow_bastion_to_eks" {
+  security_group_id = module.eks.cluster_primary_security_group_id
+
+  referenced_security_group_id = aws_security_group.bastion_sg.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "Allow Bastion host to communicate with EKS API server"
+}
 
 #########################################
 # Bastion IAM
